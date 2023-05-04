165b4262b3bd0259be704ca77c0ae951f378e004
Malek <johnmalek54@gmail.com>
2023-05-04 22:22:31 +0000

    All files

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,7 +5,7 @@ const apiEndpoint = 'https://swapi-api.alx-tools.com/api/films';
 movieNo = process.argv[2]
 
 request(apiEndpoint + `/${movieNo}`, function (err, res) {
-  data = res.body;
+  const data = JSON.parse(res.body);
   console.log(data)
   console.log('error: ', err);
   console.log('statusCode: ', res && res.statusCode);
