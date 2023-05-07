b98339e4a0d8e861f2262c9968857b7c4815b02a
Malek <johnmalek54@gmail.com>
2023-05-07 11:57:29 +0000

    Correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,12 +1,54 @@
 #!/usr/bin/node
+
 const request = require('request');
-const apiEndpoint = 'https://swapi-api.alx-tools.com/api/films';
 
-movieNo = process.argv[2]
+const movieId = process.argv[2];
 
-request(apiEndpoint + `/${movieNo}`, function (err, res) {
-  const data = JSON.parse(res.body);
-  console.log(data)
-  console.log('error: ', err);
-  console.log('statusCode: ', res && res.statusCode);
-});
+const apiEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+let people = [];
+const names = [];
+
+const requestCharacters = async () => {
+  await new Promise(resolve => request(apiEndpoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCde: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
+    }
+  }));
+};
+
+const requestNames = async () => {
+  if (people.length > 0) {
+    for (const person of people) {
+      await new Promise(resolve => request(person, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
+  } else {
+    console.error('Error: Got no Characters');
+  }
+};
+
+const getCharacterNames = async () => {
+  await requestCharacters();
+  await requestNames();
+
+  for (const name of names) {
+    if (name === names[names.length - 1]) {
+      process.stdout.write(name);
+    } else {
+      process.stdout.write(name + '\n');
+    }
+  }
+};
+
+getCharacterNames();
